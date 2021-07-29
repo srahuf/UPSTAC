1e0a3c0efd923016e29dd9c48fb103cbc7e3e559
srahuf <srahuf@gmail.com>
2021-07-29 20:34:08 +0530

    Code cleanup

--- src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
+++ src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
@@ -30,9 +30,6 @@ public class LabRequestController {
 
     Logger log = LoggerFactory.getLogger(LabRequestController.class);
 
-
-
-
     @Autowired
     private TestRequestUpdateService testRequestUpdateService;
 
@@ -42,38 +39,23 @@ public class LabRequestController {
     @Autowired
     private TestRequestFlowService testRequestFlowService;
 
-
-
     @Autowired
     private UserLoggedInService userLoggedInService;
-
-
 
     @GetMapping("/to-be-tested")
     @PreAuthorize("hasAnyRole('TESTER')")
     public List<TestRequest> getForTests()  {
-
-
        return testRequestQueryService.findBy(RequestStatus.INITIATED);
-
-
-
-
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('TESTER')")
     public List<TestRequest> getForTester()  {
-
         // Implement This Method
-
         // Create an object of User class and store the current logged in user first
         //Implement this method to return the list of test requests assigned to current tester(make use of the above created User object)
         //Make use of the findByTester() method from testRequestQueryService class
         // For reference check the method getForTests() method from LabRequestController class
-
-
-
         User currentTester = userLoggedInService.getLoggedInUser();
         return testRequestQueryService.findByTester(currentTester );
     }
@@ -82,12 +64,8 @@ public class LabRequestController {
     @PreAuthorize("hasAnyRole('TESTER')")
     @PutMapping("/assign/{id}")
     public TestRequest assignForLabTest(@PathVariable Long id) {
-
-
-
         User tester =userLoggedInService.getLoggedInUser();
-
-      return   testRequestUpdateService.assignForLabTest(id,tester);
+        return   testRequestUpdateService.assignForLabTest(id,tester);
     }
 
     @PreAuthorize("hasAnyRole('TESTER')")
@@ -98,17 +76,10 @@ public class LabRequestController {
 
             User tester=userLoggedInService.getLoggedInUser();
             return testRequestUpdateService.updateLabTest(id,createLabResult,tester);
-
-
         } catch (ConstraintViolationException e) {
             throw asConstraintViolation(e);
         }catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
-
-
-
-
-
 }
